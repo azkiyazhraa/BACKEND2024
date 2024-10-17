1449ab1c7c77d696eb61295855a0143957186c05
azkiyazhraa <azkiyazhrani@gmail.com>
2024-10-17 15:44:22 +0700

    Pertemuan 3

---
+++ Pertemuan 3/index.php
@@ -0,0 +1,153 @@
+<?php 
+  // Variable
+  /**
+   * tidak boleh dimulai angka
+   * tidak boleh mengandung spasi
+   * tidak boleh karakter
+   */
+
+  //  $a = 10;
+  // //  variable menggunakan snack case
+  // $nama_lengkap = "Abdul Gofat";
+  // $namaLengkap = "Gofar Abdul";
+
+  // Tipe Data
+  /**
+   * String
+   * Integer
+   * Float
+   * Boolean
+   */
+
+  //  $name = "L";
+  //  $nilai = 80;
+  //  $phi = 3.14;
+  //  $true = True;
+  //  $false = False;
+
+  // Percabangan
+  // if( $nilai > 90 ){
+  //   echo "Rating anda A";
+  // } else if ($nilai >= 90) {
+  //   echo "Rating anda B";
+  // }
+  // else {
+  //   echo "Rating anda tidak A";
+  // }
+
+  // Perulangan
+  // For loop
+
+  // echo ("<br>");
+  // for( $i = 0; $i < 10; $i++) {
+  //   echo $i;
+  //   echo("<br>");
+  // }
+
+  // // Foreach
+  // $fruit1 = "apple";
+  // $fruit2 = "Melon";
+  // $fruit3 = "Mangga";
+
+  
+  
+  // $fruits = ["apple", "melon", "mangga"];
+  // foreach($fruits as $fruit) {
+  //     echo($fruit);
+  //     echo("<br>");
+  //   }
+  //   array_push($fruits, "durian");
+  //   echo("<br>");
+  //   foreach($fruits as $fruit) {
+  //       echo($fruit);
+  //       echo("<br>");
+  //     }
+      
+  //     $fruits[0] = "manggis";
+  //     echo("<br>");
+  //     foreach($fruits as $fruit) {
+  //         echo($fruit);
+  //         echo("<br>");
+  //       }
+        
+  //       unset($fruits[0]);
+  //       echo("<br>");
+  //       foreach($fruits as $fruit) {
+  //           echo($fruit);
+  //           echo("<br>");
+  //         }
+
+
+  // Fungsi
+  // $a = 0;
+  // $b = 6;
+  // $total = $a + $b;
+  // echo $total;
+
+  // function sum($a, $b) {
+  //   return $a + $b;
+  // }
+
+  // function greet() {
+  //   return "Hello World";
+  // }
+
+  // echo greet();
+
+  // echo "<br>";
+  
+  // $jumlah = sum(0, 6);
+  // $jumlah1 = sum(10, 6);
+  // echo $jumlah;
+
+  // Array
+
+
+  // OOP
+  // Class
+  // class Fruit{
+  //   // property
+  //   public $fruits;
+
+  //   // method = fungsi
+  //   // Construct
+  //   public function __construct($fruits) {
+  //     $this->fruits = $fruits;
+  //   }
+
+  //   public function index() {
+  //     foreach($this->fruits as $fruit) {
+  //       echo $fruit;
+  //       echo "<br>";
+  //     }
+  //   }
+
+  //   public function tambahBuah($fruit) {
+  //     array_push($this->fruits, $fruit);
+  //   }
+
+  //   public function editBuah($index, $fruit) {
+  //     $this->fruits[$index] = $fruit;
+  //   }
+
+  //   public function hapusBuah($index) {
+  //     unset($this->fruits[$index]);
+  //   }
+    
+  // }
+
+  // $fruits = new Fruit(["Apple", "Anggur", "Manggis"]);
+
+  // $fruits->index();
+  // $fruits->tambahBuah("Jeruk");
+  // echo "<br>";
+  // $fruits->index();
+  // echo"<br>";
+  // $fruits->editBuah(0, "Durian");
+  // $fruits->index();
+  // $fruits->hapusBuah(2);
+  // $fruits->index();
+
+
+  
+?>
